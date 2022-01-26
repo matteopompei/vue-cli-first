891ff44ab962160e0a68701e36a85a9698e0816e
Matteo Pompei <datxizzan@gmail.com>
2022-01-26 16:41:31 +0100

    Title creato

---
+++ src/components/Title.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>La mia prima app con Vue CLI!</div>
+</template>
+
+<script>
+export default {
+  name: "Title",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
